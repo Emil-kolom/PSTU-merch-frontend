fce685075435e66a7f8c891a41b2112cb8af4b61
Emil-kolom <82023090+Emil-kolom@users.noreply.github.com>
2022-11-16 22:51:33 +0200

    add size block to ProductPage

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -26,26 +26,42 @@ const ProductPage = () => {
                 <div className={'carousel'}>
                     <Carousel wrapAround={true}
                               className={'slide'}
-                              renderCenterLeftControls={({ previousDisabled, previousSlide }) => (
+                              renderCenterLeftControls={({previousDisabled, previousSlide}) => (
                                   <LeftArrow onClick={previousSlide} disabled={previousDisabled}/>
                               )}
-                              renderCenterRightControls={({ nextDisabled, nextSlide }) => (
+                              renderCenterRightControls={({nextDisabled, nextSlide}) => (
                                   <LeftArrow onClick={nextSlide} disabled={nextDisabled}
-                                  style={{transform: 'scaleX(-1)'}}/>
+                                             style={{transform: 'scaleX(-1)'}}/>
                               )}
                     >
                         {
                             imgSetPath.map((imgPath, i) => {
-                                return <img style={{userSelect: 'none'}} src={process.env.PUBLIC_URL + imgPath} alt={imgPath} key={i}/>
+                                return <img style={{userSelect: 'none'}} src={process.env.PUBLIC_URL + imgPath}
+                                            alt={imgPath} key={i}/>
                             })
                         }
                     </Carousel>
                 </div>
             </div>
-            { productInfo.inStock.clothesSizes.length > 0?
+            {productInfo.inStock.clothesSizes.length > 0 ?
                 <span className={'price'}>{productInfo.inStock.price[currentSize] + ' Грн'}</span>
                 :
                 <span>Товару немає в наявності</span>
+            }
+            {
+                productInfo.inStock.clothesSizes.length > 0 ?
+                    <div>
+                        <h3>Size:</h3>
+                        {productInfo.inStock.clothesSizes.map((sizeValue, id) => {
+                            return <div>
+                                <input type={'radio'} name={'size'} value={'sizeValue'} id={sizeValue + '-size'} key={id}/>
+                                <label htmlFor={sizeValue + '-size'}>{sizeValue}</label>
+                            </div>
+                        })
+                        }
+                    </div>
+                    :
+                    null
             }
             <p className={'productDescription'}>{productInfo.description}</p>
         </main>
